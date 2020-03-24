b7f40e692648909ea9ed4c4591133b04f22aaa02
oliver7100 <59563775+oliver7100@users.noreply.github.com>
2020-03-24 15:10:50 +0100

    Updated snake game

--- Snake Game/Snake Game/Program.cs
+++ Snake Game/Snake Game/Program.cs
@@ -8,6 +8,34 @@ namespace Snake_Game
 {
     class Program
     {
+
+        int height = 20;
+        int width = 30;
+
+        public void Board()
+        {
+
+            Console.Clear();
+            for (int i = 1; i <=(width) ; i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (height+2));
+                Console.Write("-");
+            }
+            for (int i = 1; i <= (height + 1); i++)
+            {
+                Console.SetCursorPosition(i, (1 + i);
+                Console.Write("-");
+            }
+
+        }
+
+
+
         static void Main(string[] args)
         {
         }
